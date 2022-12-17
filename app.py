34c3477e74319a9072bc3b9c48c4a6ec027ef217
NepI2 <80046487+NepI2@users.noreply.github.com>
2022-12-18 00:32:22 +0100

    Working base64 decoding but need to encode emotionalitites strings to be able to read it in Java

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
+import base64
+
 import cv2
 import jsonpickle
+import urllib.parse
 import numpy as np
+from PIL import Image
+import mat
 from deepface import DeepFace
+from matplotlib import pyplot as plt
+from urllib.parse import urlparse
 from flask import Flask, request, Response
+
 
 app = Flask(__name__)
 
@@ -11,20 +19,28 @@ app = Flask(__name__)
 def test():
     print("request recieved")
     r = request
-    sstring = r.data
-    imageString = r.data.__str__().split("=")[1]
-    #imageString = r.data.decode("UTF-8").split("=")[1]
+    data = urllib.parse.unquote_plus(r.data.decode(encoding='UTF-8', errors='strict'))
+    image = data[5:len(data)]
+    # imageString = r.data.decode("UTF-8").split("=")[1]
     # convert string of image data to uint8
-    nparr = np.fromstring(imageString, np.uint8)
+    jpg_original = base64.b64decode(image)
+    nparr = np.frombuffer(jpg_original, dtype=np.uint8)
+    #nparr = np.frombuffer(image, dtype="uint8")
+    # nparr = mat.Mat.from_string(image)
+
+    # img = Image.fromarray(nparr, 'RGB')
+    # img.show()
+    plt.imshow(nparr, interpolation='nearest')
+    plt.show()
     print("data transformed")
     try:
-        img = cv2.imdecode(nparr, 0)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     except:
         print("data transformed")
     if img is not None:
-        emotionalities = DeepFace.analyze(img, tuple('emotion'))
+        emotionalities = DeepFace.analyze(img, actions=["emotion"], enforce_detection=False)
         print(emotionalities)
-        response = {'message': emotionalities[0]}
+        response = {'message': emotionalities}
         response_pickled = jsonpickle.encode(response)
     else:
         response_pickled = jsonpickle.encode({'message': 'image was empty'})
